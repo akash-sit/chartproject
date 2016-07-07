07f50f66172a2c49c4d027c91acdc6458cf0068c
akash-sit <akash.biswas@fusioncharts.com>
2016-07-07 12:20:17 +0530

    Range of y and ticks of x
    X-axis number of ticks and Y-axis range calculations done.

--- charts.js
+++ charts.js
@@ -7,71 +7,90 @@ var chartDetails=function(a,b)
   this.subCaption=b;
 }
 
-var chartxAxis = function(a,b)
+var chartxAxis = function(a,b,no)
 {
   this.label=a;
   this.value=b;
+  this.no_ticks=no;
 }
 
-var chartyAxis = function(a,b)
+var chartyAxis = function(a,b,min,max)
 {
   this.label=a;
   this.value=b;
+  this.min=min;
+  this.max=max;
+  this.range=max - min;
 }
 
 function loadChartData()
 {
-xmlhttp.onreadystatechange = function()
-{
-    if(xmlhttp.readyState === 4 && xmlhttp.status === 200) {
-      json = JSON.parse(xmlhttp.responseText);
+    xmlhttp.onreadystatechange = function()
+    {
+        if(xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+          json = JSON.parse(xmlhttp.responseText);
 
-      console.log("Actual json");
-      console.log(xmlhttp.responseText);
-      console.log("Fetched json");
-      console.log(json);
+          console.log("Actual json");
+          console.log(xmlhttp.responseText);
+          console.log("Fetched json");
+          console.log(json);
 
-      var data = document.getElementById("showData");
+          var data = document.getElementById("showData");
 
-      data.innerHTML = 'Chart Loaded <br> The Caption  of the chart is : ' + json.chartCaption + '<br>';
-      data.innerHTML += 'The sub caption of the chart is : ' + json.chartSubCaption + '<br>';
-
-
-      //Chart caption and sub caption
-      var chartnames = new chartDetails(json.chartCaption,json.chartSubCaption);
-      //console.log(chartnames);
-      var temp = [];
-      var temp1 = [];
+          data.innerHTML = 'Chart Loaded <br> The Caption  of the chart is : ' + json.chartCaption + '<br>';
+          data.innerHTML += 'The sub caption of the chart is : ' + json.chartSubCaption + '<br>';
 
 
-      //Chart details of x-axis
-      var i;
-      var no = Object.keys(json.xdata).length;
-      for(i = 0 ; i < no ; i++)
-      {
-                temp[i] = json.xdata[i];
-      }
-      var chartx = new chartxAxis(json.xAxisLabel, temp);
+          //Chart caption and sub caption
+          var chartnames = new chartDetails(json.chartCaption,json.chartSubCaption);
+          //console.log(chartnames);
+                    
+
+          //Chart details of x-axis
+          var temp = [];
+          var i;
+          var no = Object.keys(json.xdata).length;
+
+          for(i = 0 ; i < no ; i++)
+          {
+                    temp[i] = json.xdata[i];
+          }
+          var chartx = new chartxAxis(json.xAxisLabel, temp, no);
 
 
-      //Chart details of y-axis
-      var charts = new Array();
-      var no_charts = Object.keys(json.yAxisLabel).length;
-      for(i = 0 ; i < no_charts ; i++)
-      {        
-                var ylabel = json.yAxisLabel[i];
-                temp1 = json.ydata[i];
-                
-                charts[i] = new chartyAxis(ylabel, temp1);
-      }
-      console.log("Parsed Internal Data from Fetched Data");
-      console.log(chartnames);
-      console.log(chartx);
-      console.log(charts);
+          //Chart details of y-axis
+          var charts = new Array();
+          var temp1 = [];
+          var j;
+          var no_charts = Object.keys(json.yAxisLabel).length;
+
+          for(i = 0 ; i < no_charts ; i++)
+          {                
+                    var ylabel = json.yAxisLabel[i];
+                    temp1 = json.ydata[i];
+
+                    var min = temp1.yvalues[0];
+                    var max = temp1.yvalues[0];
+                    
+                    for(j = 0 ; j < temp1.yvalues.length ; j++)
+                    {
+                      if(temp1.yvalues[j] < min)
+                        min = temp1.yvalues[j];
+                      if(temp1.yvalues[j] > max)
+                        max = temp1.yvalues[j];
+                    }
+
+                    charts[i] = new chartyAxis(ylabel, temp1, min, max);
+          }
+          console.log("Parsed Internal Data from Fetched Data");
+          console.log(chartnames);
+          console.log(chartx);
+          console.log(charts);
+
+          //call functions
+        }
     }
 }
-}
-
 
 xmlhttp.open('GET','structure.json',true);
 xmlhttp.send();
